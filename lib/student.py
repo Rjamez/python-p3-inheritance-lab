ef0795748289f4b324688f6f21df7d5c0ea59e86
Rjamez <robin.adhola.student@moringaschool.com>
2024-12-03 08:28:56 +0300

    initial commit

--- lib/student.py
+++ lib/student.py
@@ -2,7 +2,10 @@
 
 from user import User
 
-class Student(User):
-    
-    def learn(self):
-        pass
+class Student(User):  
+    def __init__(self, first_name, last_name):  
+        super().__init__(first_name, last_name)  # Initialize the User part  
+        self.knowledge = []  # Empty list for student's knowledge  
+
+    def learn(self, info):  
+        self.knowledge.append(info)  # Add the learned string to knowledge
--- lib/teacher.py
+++ lib/teacher.py
@@ -4,7 +4,21 @@ from user import User
 
 import random
 
-class Teacher(User):
+class Teacher(User):  
+    knowledge = [  
+        "str is a data type in Python",  
+        "programming is hard, but it's worth it",  
+        "JavaScript async web request",  
+        "Python function call definition",  
+        "object-oriented teacher instance",  
+        "programming computers hacking learning terminal",  
+        "pipenv install pipenv shell",  
+        "pytest -x flag to fail fast",  
+    ]  
 
-    def teach(self):
-        pass
+    def __init__(self, first_name, last_name):  
+        super().__init__(first_name, last_name)  # Initialize the User part  
+        self.knowledge = Teacher.knowledge  # Store the knowledge list  
+
+    def teach(self):  
+        return random.choice(self.knowledge)  # Return a random knowledge piece
